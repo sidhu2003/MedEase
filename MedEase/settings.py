e946eb386f834b6fc07e034fa5b26dd6fe851ae4
Venkata Siddardha Rali <sidhurv8@gmail.com>
2024-04-17 18:54:42 +0530

    changes

--- MedEase/settings.py
+++ MedEase/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'MedEase.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pgdb',
+        'NAME': 'postgres',
         'USER': 'admin',
         'PASSWORD': 'admin',
         'HOST': 'pgdb',
