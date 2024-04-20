64ecae2a0db6a5b28c965e039c91dfd2d7221695
Venkata Siddardha Rali <sidhurv8@gmail.com>
2024-04-20 18:54:57 +0530

    deleted legacy_version

--- legacy_version (static)/terraform/setup.sh
+++
@@ -1,9 +0,0 @@
-!#bin/bash 
-
-sudo apt update
-sudo apt install apache2 -y
-sudo apt install git -y
-
-git clone https://github.com/sidhu2003/medease-devops
-cd medease-devops && cd src 
-cp -r * /var/www/html
