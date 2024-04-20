444a600c390862e975dcf64d8ff4dfb9dadc81fb
Venkata Siddardha Rali <sidhurv8@gmail.com>
2024-04-20 18:53:44 +0530

    terraform deployment added

--- terraform/sec_grp.tf
+++ terraform/sec_grp.tf
@@ -9,6 +9,12 @@ resource "aws_security_group" "allow_http_traffic" {
 
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_http_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
 resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
   security_group_id = aws_security_group.allow_http_traffic.id
   cidr_ipv4         = "0.0.0.0/0"
---
+++ terraform/setup.sh
@@ -0,0 +1,30 @@
+!#/bin/bash
+# only for ubuntu users
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker Engine
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt install docker-compose -y
+
+# Add user to docker group
+sudo usermod -aG docker $USER
+
+# git clone
+git clone https://github.com/sidhu2003/medease-devops
+cd medease-devops
+# Docker Compose Up
+sudo  docker-compose -f docker-compose-production.yml up -d
+
+# Migrate the Database
+sudo docker exec -it django-app python3 manage.py migrate
